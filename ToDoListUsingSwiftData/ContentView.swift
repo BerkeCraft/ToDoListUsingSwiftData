3380db658f185a8ab26de8ad7e72a5a5f461d143
BerkeCraft <berkecora@hotmail.com>
2024-11-03 15:17:49 +0300

    Initial Commit

---
+++ ToDoListUsingSwiftData/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToDoListUsingSwiftData
+//
+//  Created by Berke Cora on 3.11.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToDoListUsingSwiftData/ToDoListUsingSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListUsingSwiftDataApp.swift
+//  ToDoListUsingSwiftData
+//
+//  Created by Berke Cora on 3.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListUsingSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
